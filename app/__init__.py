cc32d69fb5899217edcda0ca16352a451335f87f
Willmo103 <88165849+Willmo103@users.noreply.github.com>
2023-08-20 21:51:55 -0500

    moving quick

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,22 @@
-from .utils.json_utils import init_json as init_json, read_json as read_json
+# Filepath: /app/__init__.py
+# Module: app
+"""
+__init__.py
+Filepath: /app/__init__.py
+Module: app
+"""
+import os
 
+# Determine the root path of the app
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# if this is installed as a dependency, the config file will be in the root of the project
+# if this is run as a standalone project, the config file will be in the root of the app
+if os.path.exists(os.path.join(APP_ROOT, "net-utils.config.json")):
+    CONFIG_PATH = os.path.join(APP_ROOT, "net-utils.config.json")
+else:
+    CONFIG_PATH = os.path.join(os.path.dirname(APP_ROOT), "net-utils.config.json")
+print("config path ", CONFIG_PATH)
+print("app root ", APP_ROOT)
+# load the environment variables
+from .utils.json_utils import init_json as init_json, read_json as read_json
--- app/utils/json_utils.py
+++ app/utils/json_utils.py
@@ -1,10 +1,28 @@
+"""
+A module to handle the json config file
+Filepath: /app/utils/json_utils.py
+Module: app.utils.json_utils
+
+functions:
+    init_json(path: str) -> bool or ConfigError
+    read_json(path: str) -> dict
+    load_ip_address_list() -> list
+
+classes:
+    ConfigError(Exception)
+
+"""
 import json
 from dotenv import load_dotenv
 
 # load the environment variables
 load_dotenv()
 
+# create a base url for net-utils.config.json
+#  - get the root of the application location
+
 class ConfigError(Exception):
+    """An exception to be raised when there is an error with the config file"""
     pass
 
 # a function to create the config json file on install
@@ -35,9 +53,19 @@ def read_json(path: str) -> dict:
         print("No config file found. run 'net-utils init' to create one")
         raise ConfigError("Error reading config file")
 
-def load_ip_address_list() -> list:
+# a function to write to the config json file
+def load_ip_address_list(path: str) -> list:
     """Loads the ip address list from the config file
     :return: the ip address list
     """
-    data = read_json("config.json")
+    data = read_json(path)
     return data["ip_address_list"]
+
+def save_ip_address_list(path: str, ip_address_list: list) -> None:
+    """Saves the ip address list to the config file
+    :param ip_address_list: the ip address list to save
+    """
+    data = read_json(path)
+    data["ip_address_list"] = ip_address_list
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
