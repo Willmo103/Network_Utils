a5e9dd6424c91c1b40236beba31fd6355e276510
Willmo103 <88165849+Willmo103@users.noreply.github.com>
2023-08-24 18:24:24 -0500

    send email working

--- run.py
+++ run.py
@@ -31,24 +31,3 @@
 
 # send_email('Hello', 'How are you?\n this is a test')
 
-import smtplib, os
-from email.mime.text import MIMEText
-
-subject = "Email Subject"
-body = "This is the body of the text message"
-sender = "netutils103@gmail.com"
-recipients = ["willmorris103@gmail.com", "sparx103@gmail.com"]
-password = os.environ.get('EMAIL_PASSWORD')
-
-def send_email(subject, body, sender, recipients, password):
-    msg = MIMEText(body)
-    msg['Subject'] = subject
-    msg['From'] = sender
-    msg['To'] = ', '.join(recipients)
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
-       smtp_server.login(sender, password)
-       smtp_server.sendmail(sender, recipients, msg.as_string())
-    print("Message sent!")
-
-
-send_email(subject, body, sender, recipients, password)
--- src/app/utils/utils.py
+++ src/app/utils/utils.py
@@ -1,4 +1,34 @@
 import os
-from app import APP_ROOT
+import smtplib, os
+from email.mime.text import MIMEText
+from dotenv import load_dotenv
+load_dotenv()
 
-# print(APP_ROOT)
+def send_email(subject: str, body: any, recipients: list[str]) -> None:
+    """
+    Sends an email
+
+    :param subject: the subject of the email
+    :param body: the body of the email
+    :param recipients: a list of email addresses to send the email to
+    :return: None
+    """
+    sender = os.environ.get('EMAIL_ADDRESS')
+    password = os.environ.get('EMAIL_PASSWORD')
+    server = os.environ.get('EMAIL_SMTP_SERVER')
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = ', '.join(recipients)
+
+    with smtplib.SMTP_SSL(server, 465) as smtp_server:
+       smtp_server.login(sender, password)
+       smtp_server.sendmail(sender, recipients, msg.as_string())
+    print("Message sent!")
+
+def gather_email_info(*args, **kwargs):
+    ...
+
+def format_email(*args, **kwargs):
+    ...
+
